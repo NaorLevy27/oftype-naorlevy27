24fdc2bfca111eb2790d2709b5302c2a8a1f775f
Naor Levy <naorlevi27@gmail.com>
2025-06-26 00:06:26 +0300

    TypeOfClass created. Funtion getOfType created too

---
+++ OfType/OfType/OfTypeClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfType
+{
+    public class OfTypeClass : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            if(enumerable == null)
+            {
+                throw new ArgumentNullException("Insserted null IEnumerable. Invalid");
+            }
+            IEnumerable<T> result = new List<T>();
+            foreach(var item in enumerable)
+            {
+                if(typeof(T).Equals(item.GetType()))
+                {
+                    result.Append<T>((T)item);
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable);
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase;
+    }
+}
